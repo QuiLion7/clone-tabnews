f356f973dcaa9ad977e76df93c44021fdb1119f4
Quilion Batista de Oliveira <85176758+QuiLion7@users.noreply.github.com>
2023-08-14 01:48:06 +0000

    adiciona scripts `lint:check` e `lint.fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do https://www.tabnews.com.br para o https://curso.dev do @filipedeschamps como método de estudo prático
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Quilion, mantenha o controle, não seja imediatista e siga as regras!</h1>
+  return (
+    <h1>
+      Quilion, mantenha o controle, não seja imediatista e siga as regras!
+    </h1>
+  );
 }
 
 export default Home;
